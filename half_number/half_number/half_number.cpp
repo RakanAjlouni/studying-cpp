92792ba991218e06fef4bdcb8bfe83d2d04b4ded
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-02-01 22:12:53 +0300

    half number added

---
+++ half_number/half_number/half_number.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    short number;
+
+    cout << "Enter a number: \n";
+
+    cin >> number;
+    cout << "\n";
+
+    short half_number = number / 2;
+
+    cout << "Half of " << number << " is " << half_number << "\n";
+
+
+    return 0;
+
+}
