5467a88529f269df400307a1491945eb00fbf070
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-02-01 12:12:30 +0300

    Airthmetic operators added

---
+++ arithmetic_operators/arithmetic_operators.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    const unsigned short A = 10, B = 20;
+
+    cout << "A + B = " << A + B << "\n";
+    cout << "A - B = " << A - B << "\n";
+    cout << "A * B = " << A * B << "\n";
+    cout << "A / B = " << A / B << "\n";
+    cout << "A % B = " << A % B << "\n";
+
+
+
+
+
+
+
+    return 0;
+}
