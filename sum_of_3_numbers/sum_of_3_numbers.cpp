3576527f44b830a229b2205b348b02c7394438c8
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-02-01 23:50:46 +0300

    sum of 3 numbers added

---
+++ sum_of_3_numbers/sum_of_3_numbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+	
+	short number1, number2, number3;
+
+	cout << "Enter number 1, number 2, number 3: \n";
+
+	cin >> number1 >> number2 >> number3;
+	cout << "\n";
+
+	short sum = number1 + number2 + number3;
+
+	cout << sum << "\n";
+	
+	
+	return 0;
+}
