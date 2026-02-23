019b54f1348e7866b41c36e86df4976ea671f6d4
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-02-24 01:37:21 +0300

    logical operators

---
+++ logical_operators/logical_operators.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    bool A = 1, B = 0;
+
+    cout << (A && B) << "\n";
+    cout << (A || B) << "\n";
+    cout << !A << "\n";
+    cout << !B << "\n";
+    cout << !(A && B) << "\n";
+    cout << !(A || B) << endl;
+
+
+
+
+    return 0;
+}
