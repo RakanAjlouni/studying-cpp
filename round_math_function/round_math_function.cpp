00b4cc29fb3346dab5dbec6b53a7f2018c7182b0
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-02-25 00:50:40 +0300

    round math function

---
+++ round_math_function/round_math_function.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+    cout << "Round value of 2.4 is: " << round(2.4) << "\n"; //gives 2
+    cout << "Round value of 2.5 is: " << round(2.5) << "\n"; //gives 3
+    cout << "Round value of 2.6 is: " << round(2.7) << "\n"; // gives 3
+
+    cout << "Square root value of 50 is: " << sqrt(50) << "\n"; //gives 7.07107
+    cout << "Round value of sqrt(5) is: " << round(sqrt(50)) << "\n"; // gives 7
+
+
+
+    return 0;
+}
