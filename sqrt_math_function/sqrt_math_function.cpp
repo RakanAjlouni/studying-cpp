97b9d87c6ceaa5196feb00f415bb1cac1c7128f9
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-02-25 00:11:28 +0300

    sqrt math function

---
+++ sqrt_math_function/sqrt_math_function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+
+int main()
+{
+    double x = 64;
+    cout << "Square root value of 64 is: " << sqrt(x) << "\n"; // gives 8
+    cout << "Square root value of 50 is: " << sqrt(50) << "\n"; // gives 7.07107
+
+
+
+    return 0;
+}
