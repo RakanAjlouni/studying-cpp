9bf9abaf8fe1bc104f825ea76ef63394116a9868
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-02-25 09:09:56 +0300

    ceil and floor functions

---
+++ ceil_and_floor_math_functions/ceil_and_floor_math_functions.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+
+int main()
+{
+	cout << "Ceiling value of 2.9 is: " << ceil(2.9) << "\n"; // gives 3
+	cout << "Floor value of 2.9 is: " << floor(2.9) << "\n"; // gives 2
+
+	cout << "Ceiling value of -2.9 is: " << ceil(-2.9) << "\n"; // gives -2
+	cout << "Floor value of -2.9 is: " << floor(-2.9) << "\n"; // gives -3
+
+
+
+
+	return 0;
+}
