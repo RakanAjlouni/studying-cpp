d823d85f0a5aff15f3d083c6ee0e4ad5610c8a64
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-02-25 09:12:57 +0300

    absolute function

---
+++ abs_math_function/abs_math_function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+	cout << "Absolute value of -10 is: " << abs(-10) << "\n"; // gives 10
+	cout << "Absolute value of 10 is: " << abs(10) << "\n"; // gives 10
+
+
+
+	return 0;
+}
