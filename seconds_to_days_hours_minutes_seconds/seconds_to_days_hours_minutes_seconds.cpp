75aec1b23926f0d1d4edda0c03ad4cda6e92d7fd
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-03-04 23:40:13 +0300

    Rounded seconds to days, hours, minutes, and seconds

--- seconds_to_days_hours_minutes_seconds/seconds_to_days_hours_minutes_seconds.cpp
+++ seconds_to_days_hours_minutes_seconds/seconds_to_days_hours_minutes_seconds.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 
@@ -6,29 +7,29 @@ using namespace std;
 int main()
 {
     unsigned int total_seconds;
-    const int seconds_per_day = 24 * 60 * 60;
-    const int seconds_per_hour = 60 * 60;
-	const int seconds_per_minute = 60;
+    const int seconds_per_day = 24 * pow(60, 2);
+    const int seconds_per_hour = pow(60, 2);
+    const int seconds_per_minute = 60;
 
     cout << "Please enter seconds:\n";
-    
+
     cin >> total_seconds;
-    
+
     unsigned int days, hours, minutes, seconds_left, remainder;
 
     days = (total_seconds / seconds_per_day),
-                 remainder = total_seconds % seconds_per_day;
+        remainder = total_seconds % seconds_per_day;
 
     hours = (remainder / seconds_per_hour),
-                 remainder = remainder % seconds_per_hour;
+        remainder = remainder % seconds_per_hour;
 
     minutes = (remainder / seconds_per_minute),
-                 remainder = remainder % seconds_per_minute;
+        remainder = remainder % seconds_per_minute;
 
     seconds_left = remainder;
 
 
-    cout << days << ":" << hours << ":" << minutes << ":" << seconds_left << endl;
+    cout << days << ":" << round(hours) << ":" << round(minutes) << ":" << round(seconds_left) << endl;
 
     return 0;
 }
