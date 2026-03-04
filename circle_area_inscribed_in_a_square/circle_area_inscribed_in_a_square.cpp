d2a084cd5a09871b46696c6c8ee91c80a830aa54
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-03-04 22:59:36 +0300

    Ceield area inscribed in a square

--- circle_area_inscribed_in_a_square/circle_area_inscribed_in_a_square.cpp
+++ circle_area_inscribed_in_a_square/circle_area_inscribed_in_a_square.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 
@@ -7,13 +8,13 @@ int main()
     unsigned short a;
     const double pi = 3.14;
 
-    cout << "Please enter D: \n";
+    cout << "Please enter A: \n";
     cin >> a;
     cout << "\n";
 
-    double circle_area_inscribed_in_square = (pi * a * a) / 4;
-    
-    cout << "Circle area inscribed in square = " << circle_area_inscribed_in_square << "\n";
+    double circle_area_inscribed_in_square = (pi * pow(a, 2)) / 4;
+
+    cout << "Circle area inscribed in square = " << ceil(circle_area_inscribed_in_square) << "\n";
 
 
     return 0;
