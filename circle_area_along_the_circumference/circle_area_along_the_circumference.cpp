ec1946b7ce6572fe6b1edcb8e79ed82f73b8d62e
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-03-04 23:02:28 +0300

    Floored circle area through the circumference

--- circle_area_along_the_circumference/circle_area_along_the_circumference.cpp
+++ circle_area_along_the_circumference/circle_area_along_the_circumference.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 
@@ -8,12 +9,12 @@ int main()
     const double pi = 3.14;
 
     cout << "Please enter L: \n";
-    cin >> l; 
+    cin >> l;
     cout << "\n";
 
-    double circle_area_along_the_circumference = (l * l) / (4 * pi);
+    double circle_area_along_the_circumference = pow(l, 2) / (4 * pi);
 
-    cout << "Circle area along the circumference = " << circle_area_along_the_circumference << "\n";
+    cout << "Circle area along the circumference = " << floor(circle_area_along_the_circumference) << "\n";
 
 
     return 0;
