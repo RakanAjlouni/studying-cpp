5eb3f12f060b32f31e627ff685e8f8b354026fc7
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-03-09 23:04:24 +0300

    Updated data conversion with implicit and two explicit styles from float to integer

--- data_types_conversion_homework/data_types_conversion_homework.cpp
+++ data_types_conversion_homework/data_types_conversion_homework.cpp
@@ -22,10 +22,12 @@ int main()
     float float_num = 55.23;
 
     string float_str;
-    int int_str;
+    int float_int;
 
     float_str = to_string(float_num);
-    int_str = (int)float_num;
+    float_int = float_num; // Implicit conversion from float to integer
+    float_int = (int)float_num;  //explicit conversion, called c style
+    float_int = int(float_num); // explicit conversion
 
 
     cout << "String to num_int = " << num_int << "\n";
@@ -34,7 +36,7 @@ int main()
     cout << "Integer to string = " << int_num << "\n";
     cout << "Double to string = " << double_num << "\n";
     cout << "Float to string = " << float_str << "\n";
-    cout << "Float to integer = " << int_str << "\n";
+    cout << "Float to integer = " << float_int << "\n";
 
 
 
