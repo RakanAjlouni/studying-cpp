b622c030304a9da8b428698730a0ad23611edccc
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-03-09 22:53:09 +0300

    Data conversion homework

---
+++ data_types_conversion_homework/data_types_conversion_homework.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+    string st1 = "43.22";
+
+    int num_int = stoi(st1);
+    float num_float = stof(st1);
+    double num_double = stod(st1);
+
+    int int_num = 20;
+    double double_num = 33.5;
+
+    string string1, string2; 
+
+    string1 = to_string(int_num);
+    string2 = to_string(double_num);
+
+    float float_num = 55.23;
+
+    string float_str;
+    int int_str;
+
+    float_str = to_string(float_num);
+    int_str = (int)float_num;
+
+
+    cout << "String to num_int = " << num_int << "\n";
+    cout << "String to num_float = " << num_float << "\n";
+    cout << "String to num_double = " << num_double << "\n";
+    cout << "Integer to string = " << int_num << "\n";
+    cout << "Double to string = " << double_num << "\n";
+    cout << "Float to string = " << float_str << "\n";
+    cout << "Float to integer = " << int_str << "\n";
+
+
+
+
+    return 0;
+}
