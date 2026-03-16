c7e91cbc873d31066286e020e0e671fd9315b328
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-03-16 03:33:59 +0300

    Functions

---
+++ functions/functions.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void my_function()
+{
+	cout << "This is my first function, it got executed!!" << "\n";
+}
+
+void my_function2()
+{
+	cout << "This is my second function!!" << "\n";
+}
+
+int main()
+{
+	my_function();
+	my_function2();
+	
+	return 0;
+}
