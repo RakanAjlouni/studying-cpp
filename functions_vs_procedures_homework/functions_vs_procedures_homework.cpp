f8e89ef0eb1ae2cdb0843c80af1afd49d3cf2973
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-03-31 19:42:03 +0300

    functions versus procedures homework

---
+++ functions_vs_procedures_homework/functions_vs_procedures_homework.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void my_sum_procedure()
+{
+    short number1, number2;
+
+    cout << "Please enter number 1: \n";
+    cin >> number1;
+
+    cout << "Please enter number 2: \n";
+    cin >> number2;
+
+    short sum = number1 + number2;
+
+    cout << "******************************\n\n" << "The sum of number 1 and number 2 is: " << sum << "\n\n";
+
+}
+
+short my_sum_function()
+{
+    short number1, number2;
+
+    cout << "Please enter number 1: \n";
+    cin >> number1;
+
+    cout << "Please enter number 2: \n";
+    cin >> number2;
+
+    cout << "******************************\n\n" << "The sum of number 1 and number 2 is: ";
+
+
+    return  number1 + number2;
+
+
+}
+
+int main()
+{
+    
+    my_sum_procedure();
+    cout << my_sum_function() + 10  << "\n\n";
+
+
+    return 0;
+}
