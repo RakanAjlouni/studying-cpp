373018267b487da014c42c4d05c8a01b6eeefd98
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-04-01 17:04:24 +0300

    Rectangle area using function homework

--- rectangle_area/rectangle_area.cpp
+++ rectangle_area/rectangle_area.cpp
@@ -1,17 +1,24 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
+
+unsigned short rectangle_area(unsigned short a, unsigned short b)
+{
+    cout << "Rectangle area = ";
+
+    return a * b;
+}
 
 int main()
 {
     unsigned short a, b;
     cout << "Enter a and b: \n\n";
-    
+
     cin >> a >> b;
     cout << "\n";
 
-    unsigned short rectangle_area = a * b;
-
-    cout << "Rectangle area = " << rectangle_area << "\n";
+    cout << rectangle_area(a, b) << "\n";
 
 
     return 0;
