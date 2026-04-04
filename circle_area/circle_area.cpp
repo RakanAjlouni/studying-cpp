cecfc2d61338b5266e0ad0e4bdb8f6ac32ace324
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-04-04 18:13:52 +0300

    Circle are updated using function

--- circle_area/circle_area.cpp
+++ circle_area/circle_area.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
 #include <cmath>
+#include <string>
+
 using namespace std;
 
+
+double circle_area(unsigned short r)
+{
+    const double pi = 3.14;
+
+    cout << "Circle area through = ";
+
+    return pi * pow(r, 2);
+
+}
 int main()
 {
     unsigned short r;
-    const double pi = 3.14;
 
     cout << "Please enter D: \n";
 
     cin >> r;
     cout << "\n";
 
-    double circle_area_through_diameter = pi * pow(r, 2);
 
-    cout << "Circle area through diameter = " << ceil(circle_area_through_diameter) << "\n";
+    cout << "Circle area through = " << circle_area(r) << "\n";
 
 
     return 0;
