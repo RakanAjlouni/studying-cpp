4139d36929a2bc25ec46b5fdbbbc08c45930d107
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-04-15 17:11:17 +0300

    Circle area through diameter updated to function

--- circle_area_through_diameter/circle_area_through_diameter.cpp
+++ circle_area_through_diameter/circle_area_through_diameter.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 #include <cmath>
+#include <string>
+
 using namespace std;
+
+double circle_area_through_diameter(unsigned short d)
+{
+    const double pi = 3.14;
+
+    cout << "Circle area through diameter = ";
+
+    return (pi * pow(d, 2)) / 4;
+}
 
 int main()
 {
     unsigned short d;
-    const double pi = 3.14;
 
     cout << "Please enter D: \n";
 
     cin >> d;
     cout << "\n";
 
-    double circle_area_through_diameter = (pi * pow(d, 2)) / 4;
 
-    cout << "Circle area through diameter = " << ceil(circle_area_through_diameter) << "\n";
+    cout << circle_area_through_diameter(d) << "\n";
 
 
     return 0;
