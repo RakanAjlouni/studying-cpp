1cd47d47aa5a0143a362b4d74c179aa58d417964
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-04-15 17:16:41 +0300

    Circle area inscribed in a square updated to function

--- circle_area_inscribed_in_a_square/circle_area_inscribed_in_a_square.cpp
+++ circle_area_inscribed_in_a_square/circle_area_inscribed_in_a_square.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 #include <cmath>
+#include <string>
+
 using namespace std;
 
+double circle_area_inscribed_in_square(unsigned short a)
+{
+    const double pi = 3.14;
+
+    cout << "Circle area inscribed in square = ";
+
+    return (pi * pow(a, 2)) / 4;
+}
 
 int main()
 {
     unsigned short a;
-    const double pi = 3.14;
 
     cout << "Please enter A: \n";
     cin >> a;
+
     cout << "\n";
 
-    double circle_area_inscribed_in_square = (pi * pow(a, 2)) / 4;
 
-    cout << "Circle area inscribed in square = " << ceil(circle_area_inscribed_in_square) << "\n";
+    cout << circle_area_inscribed_in_square(a) << "\n";
 
 
     return 0;
