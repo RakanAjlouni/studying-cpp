725e75ac53380d5232a8d7549b73168f005ca0f8
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-04-15 17:23:37 +0300

    Circle area along the circumference updated to function

--- circle_area_along_the_circumference/circle_area_along_the_circumference.cpp
+++ circle_area_along_the_circumference/circle_area_along_the_circumference.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
 #include <cmath>
+#include <string>
+
 using namespace std;
 
+double circle_area_along_the_circumference(unsigned short l)
+{
+    const double pi = 3.14;
+
+    cout << "Circle area along the circumference = ";
+
+    return pow(l, 2) / (4 * pi);
+}
 
 int main()
 {
     unsigned short l;
-    const double pi = 3.14;
 
     cout << "Please enter L: \n";
     cin >> l;
     cout << "\n";
 
-    double circle_area_along_the_circumference = pow(l, 2) / (4 * pi);
-
-    cout << "Circle area along the circumference = " << floor(circle_area_along_the_circumference) << "\n";
-
+    cout << circle_area_along_the_circumference(l) << "\n";
 
     return 0;
 }
