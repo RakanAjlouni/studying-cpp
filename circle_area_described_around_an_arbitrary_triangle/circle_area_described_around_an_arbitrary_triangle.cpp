98926f72f93ced24a35e1e539431b86d8a56f35b
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-04-17 21:36:34 +0300

    Circle area described around an arbitrary triangle updated using function

--- circle_area_described_around_an_arbitrary_triangle/circle_area_described_around_an_arbitrary_triangle.cpp
+++ circle_area_described_around_an_arbitrary_triangle/circle_area_described_around_an_arbitrary_triangle.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <cmath>
+#include <string>
+
 using namespace std;
+
+double circle_area_described_around_an_arbitrary_triangle(unsigned short a, unsigned short b, unsigned short c)
+{
+    const double pi = 3.14;
+
+    cout << "Circle area described around an arbitrary triangle = ";
+
+    double p = (a + b + c) / 2;
+    double x = (a * b * c) / (4 * (sqrt(p * (p - a) * (p - b) * (p - c))));
+    return pi * pow(x, 2);
+}
 
 int main()
 {
     unsigned short a, b, c;
-    const double pi = 3.14;
 
     cout << "Please enter a, b, c: \n";
 
@@ -13,11 +25,7 @@ int main()
 
     cout << "\n";
 
-    double p = (a + b + c) / 2;
-    double x = (a*b*c) / (4*(sqrt(p*(p-a)*(p-b)*(p-c))));
-    double circle_area_described_around_an_arbitrary_triangle = pi * pow(x, 2);
-
-    cout << "Circle area described around an arbitrary triangle = " << round(circle_area_described_around_an_arbitrary_triangle) << "\n";
+    cout << circle_area_described_around_an_arbitrary_triangle(a, b, c) << "\n";
 
 
     return 0;
