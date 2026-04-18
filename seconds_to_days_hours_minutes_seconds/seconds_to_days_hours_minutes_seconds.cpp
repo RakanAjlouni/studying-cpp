26869f0555b6cc70feb8fc40dd372e078159c5de
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-04-18 16:57:17 +0300

    seconds to days hours minutes seconds task updated into functions

--- seconds_to_days_hours_minutes_seconds/seconds_to_days_hours_minutes_seconds.cpp
+++ seconds_to_days_hours_minutes_seconds/seconds_to_days_hours_minutes_seconds.cpp
@@ -1,35 +1,59 @@
 #include <iostream>
 #include <cmath>
+
 using namespace std;
 
+unsigned int get_days(unsigned int total_seconds)
+{
+    const unsigned int seconds_per_day = 24 * pow(60, 2);
+    return total_seconds / seconds_per_day;
+
+}
+
+unsigned int get_hours(unsigned int total_seconds)
+{
+    const unsigned int seconds_per_day = 24 * pow(60, 2);
+    const unsigned int seconds_per_hour = pow(60, 2);
+
+    unsigned int remainder = total_seconds % seconds_per_day;
+    return remainder / seconds_per_hour;
+}
+
+unsigned int get_minutes(unsigned int total_seconds)
+{
+    const unsigned int seconds_per_day = 24 * pow(60, 2);
+    const unsigned int seconds_per_hour = pow(60, 2);
+    const unsigned int seconds_per_minute = 60;
+
+    unsigned int remainder = total_seconds % seconds_per_day;
+    remainder = remainder % seconds_per_hour;
+    return remainder / seconds_per_minute;
+}
+
+unsigned int get_seconds_left(unsigned int total_seconds)
+{
+    const unsigned int seconds_per_day = 24 * pow(60, 2);
+    const unsigned int seconds_per_hour = pow(60, 2);
+    const unsigned int seconds_per_minute = 60;
+
+    unsigned int remainder = total_seconds % seconds_per_day;
+    remainder = remainder % seconds_per_hour;
+    remainder = remainder % seconds_per_minute;
+    return remainder;
+}
 
 
 int main()
 {
     unsigned int total_seconds;
-    const int seconds_per_day = 24 * pow(60, 2);
-    const int seconds_per_hour = pow(60, 2);
-    const int seconds_per_minute = 60;
 
-    cout << "Please enter seconds:\n";
+    cout << "Please enter total seconds: \n";
 
     cin >> total_seconds;
 
-    unsigned int days, hours, minutes, seconds_left, remainder;
-
-    days = (total_seconds / seconds_per_day),
-        remainder = total_seconds % seconds_per_day;
-
-    hours = (remainder / seconds_per_hour),
-        remainder = remainder % seconds_per_hour;
-
-    minutes = (remainder / seconds_per_minute),
-        remainder = remainder % seconds_per_minute;
-
-    seconds_left = remainder;
 
 
-    cout << days << ":" << round(hours) << ":" << round(minutes) << ":" << round(seconds_left) << endl;
+    cout << get_days(total_seconds) << ":" << get_hours(total_seconds) << ":" << get_minutes(total_seconds) << ":" << get_seconds_left(total_seconds) << endl;
 
     return 0;
 }
