5f68053ab41e3f0950b1fe2239b9690ba493e651
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-04-19 21:26:59 +0300

    Arrays

---
+++ arrays/arrays.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x[5] = { 22, 18, 2, 55, 520 };
+
+	cout << x[0] + x[4] << "\n";
+	cout << x[1] << "\n";
+	cout << x[2] << "\n";
+	cout << x[3] << "\n";
+	cout << x[4] << "\n\n";
+
+	int z[2];
+
+	z[0] = 10;
+	z[1] = 20;
+
+	cout << z[0] + z[1] << endl;
+
+
+	return 0;
+}
