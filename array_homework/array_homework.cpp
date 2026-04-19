6c5609cfd038479a6fe8d77d5348920228a2082a
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-04-19 23:57:44 +0300

    array homework updated to arrays and functions

--- array_homework/array_homework.cpp
+++ array_homework/array_homework.cpp
@@ -2,24 +2,38 @@
 
 using namespace std;
 
-int main()
+void read_grades(float grades[3])
 {
-    float grades[3];
-
     cout << "Please enter grade 1: \n";
     cin >> grades[0];
-    
+
     cout << "Please enter grade 2: \n";
     cin >> grades[1];
 
     cout << "Please enter grade 3: \n";
     cin >> grades[2];
-    
+}
+
+float average_of_grades(float grades[3])
+{
+    return (grades[0] + grades[1] + grades[2]) / 3;
+}
+
+void write_grades(float grades[3])
+{
+
     cout << "************************\n\n";
 
-    float average_of_grades = (grades[0] + grades[1] + grades[2]) / 3;
+    cout << "The average of grades is: " << average_of_grades(grades) << endl;
+}
 
-    cout << "The average of grades is: " << average_of_grades << endl;
+int main()
+{
+    float grades[3];
+
+    read_grades(grades);
+    write_grades(grades);
+  
 
     return 0;
 }
