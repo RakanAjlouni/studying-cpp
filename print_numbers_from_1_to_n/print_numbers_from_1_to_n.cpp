a2957e52edbe99c1f77561f96049a997939be79e
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-08-31 21:22:10 +0300

    added print numbers from 1 to n using for loop

---
+++ print_numbers_from_1_to_n/print_numbers_from_1_to_n.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int number;
+	cout << "Please enter a number: ";
+	cin >> number;
+
+	for (int i = 1; i <= number; i++)
+	{
+		cout << i << endl;
+	}
+}
