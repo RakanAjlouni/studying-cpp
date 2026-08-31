36aeccebe7d97a9c7d2d72785aefe36513f04701
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2026-08-31 21:51:10 +0300

    solved using functions and procedures

--- print_numbers_from_n_to_1/print_numbers_from_n_to_1.cpp
+++ print_numbers_from_n_to_1/print_numbers_from_n_to_1.cpp
@@ -2,15 +2,27 @@
 
 using namespace std;
 
-int main()
+int user_number()
 {
 	int number;
-	
+
 	cout << "Please enter a number: ";
 	cin >> number;
 
+	return number;
+}
+
+void for_loop(int number)
+{
 	for (int i = number; i >= 1; i--)
 	{
 		cout << i << endl;
 	}
+}
+
+int main()
+{
+	for_loop(user_number());
+
+	return 0;
 }
